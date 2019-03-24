d339c4803c8d3a81edd1abeb4667cce5da3ceffa
giustoe82 <marcog.swe@gmail.com>
2019-03-24 14:07:55 +0100

    first

---
+++ Restaurant-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant-demo
+//
+//  Created by Marco Giustozzi on 2019-03-24.
+//  Copyright © 2019 marcog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
